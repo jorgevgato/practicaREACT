5192acd69596f35457ca7a163f9da4da4bc0dc0b
Jorge Villar <jorge.vfg@gmail.com>
2025-05-26 20:48:56 +0200

    4. App Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import LoginPage from "./pages/auth/login-page";
-import { useAuth } from "./pages/auth/context";
 import AdvertsPage from "./pages/adverts/adverts-page";
+import { Navigate, Route, Routes } from "react-router";
 
 function App() {
-  const { isLogged } = useAuth();
-  return isLogged ? <AdvertsPage /> : <LoginPage />;
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/adverts" element={<AdvertsPage />} />
+      <Route path="/" element={<Navigate to="/adverts" />} />
+      {/* <Route path="adverts/new" element={<NewAdvertPage />} />*/}
+      {/* <Route path="adverts/:advertId" element={<AdvertPage />} />*/}
+      <Route path="/404" element={<div>404 | Not Found</div>} />
+      <Route path="*" element={<Navigate to="/404" />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import storage from "./utils/storage.ts";
 import { setAuthorizationHeader } from "./api/client.ts";
 import AuthProvider from "./pages/auth/auth-provider.tsx";
+import { BrowserRouter } from "react-router";
 
 const accessToken = storage.get("auth");
 if (accessToken) {
@@ -13,7 +14,9 @@ if (accessToken) {
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider defaultIsLogged={accessToken ? true : false}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </AuthProvider>
   </StrictMode>,
 );
