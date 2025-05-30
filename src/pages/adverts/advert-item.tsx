fabee16e7aa9c6eb53bd8a2c2ab9b8de5c914972
Jorge Villar <jorge.vfg@gmail.com>
2025-05-30 21:23:40 +0200

    foto por defecto

--- src/pages/adverts/advert-item.tsx
+++ src/pages/adverts/advert-item.tsx
@@ -6,10 +6,12 @@ interface AdvertItemProps {
 
 const AdvertItem = ({ advert }: AdvertItemProps) => {
   const { name, sale, price, tags, photo } = advert;
+  const defaultPhoto = "/vite.svg"
+  const imageSrc = photo || defaultPhoto
 
   return (
     <div className="advert-item-detail">
-      {photo && <img src={photo} alt={name} className="advert-photo" />}
+      <img src={imageSrc} alt={name} className="advert-photo" />
       <h3 className="advert-name">{name}</h3>
       <p className="advert-sale">
         {sale ? "Compra" : "Venta"} - {price} €
