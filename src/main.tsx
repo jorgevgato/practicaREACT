90d657702527463227c39dd64b098c06b82ddace
Jorge Villar <jorge.vfg@gmail.com>
2025-05-30 17:30:37 +0200

    6. error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import storage from "./utils/storage.ts";
 import { setAuthorizationHeader } from "./api/client.ts";
 import AuthProvider from "./pages/auth/auth-provider.tsx";
 import { BrowserRouter } from "react-router";
+import ErrorBoundary from "./components/errors/error-boundary.tsx";
 
 const accessToken = storage.get("auth");
 if (accessToken) {
@@ -13,10 +14,12 @@ if (accessToken) {
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider defaultIsLogged={accessToken ? true : false}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthProvider>
+    <ErrorBoundary>
+      <AuthProvider defaultIsLogged={accessToken ? true : false}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
+    </ErrorBoundary>
   </StrictMode>,
 );
