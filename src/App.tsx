67c865bf03b8e1d4824428be6839f88a2395ef86
Jorge Villar <jorge.vfg@gmail.com>
2025-06-01 18:35:26 +0200

    page-404

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import LoginPage from "./pages/auth/login-page";
 import AdvertsPage from "./pages/adverts/adverts-page";
-import { Navigate, Outlet, Route, Routes } from "react-router";
+import { Link, Navigate, Outlet, Route, Routes } from "react-router";
 import Layout from "./components/layout/layout";
 import RequireAuth from "./pages/auth/require-auth";
 import AdvertDetail from "./pages/adverts/advert-detail-page";
 import AdvertCreator from "./pages/adverts/advert-creator-page";
+import logo from "./assets/react.svg";
+import "./styles/page-404.css"
 
 function App() {
   return (
@@ -30,7 +32,12 @@ function App() {
         <Route path=":advertId" element={<AdvertDetail />} />
       </Route>
       <Route path="/" element={<Navigate to="/adverts" />} />
-      <Route path="/404" element={<div>404 | Not Found</div>} />
+      <Route path="/404" element={<div className="page-404">
+        <p>404 | Not Found</p>
+        <Link to="/">
+            <img src={logo} alt="Ir al inicio" className="login-logo" />
+          </Link>
+        </div>} />
       <Route path="*" element={<Navigate to="/404" />} />
     </Routes>
   );
